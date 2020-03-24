a2437e165ff2c242772d545c3d20487addcf5528
Chandra Deep Rathan K <kcdrathan@gmail.com>
2020-03-24 15:25:21 +0530

    Create User profile router

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,8 +1,28 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
+const passport = require("passport");
+
+const Profile = require("../../models/Profile")
+const User = require("../../models/User")
 
 router.get("/tests", (req, res) => {
     res.json({ msg: "profile works" });
 });
+
+router.get("/", passport.authenticate("jwt", { session: false }), (req, res) => {
+    let errors = {}
+    Profile.findOne({user: req.user.id})
+        .then( profile => {
+            if (!profile) {
+                errors.noprofile = "User has no profile"
+                return res.status(404).json(errors)
+            }
+            res.json(profile)
+        })
+        .catch(err => {
+            res.ststus(404).json(err)
+        })
+})
 
 module.exports = router;
